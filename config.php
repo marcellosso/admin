b1aa343d3ca20bb23480e64fd23989b45f0521ea
Marcel Losso Forte <marcel.losso@gmail.com>
2020-06-18 17:50:16 -0300

    Fixed API URL and ADMIN URL constant

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
 
-    define('URL_API_FUTURONERD',    'http://www.futuronerd.net/');
-//    define('URL_API_FUTURONERD',    'http://localhost:8080/');
-    define('URL_ADMIN_FUTURONERD',  'http://admin.futuronerd.net/');
+    define('URL_API_FUTURONERD',    'http://www.futuronerd.net');
+//    define('URL_API_FUTURONERD',    'http://localhost:8080');
+    // define('URL_ADMIN_FUTURONERD',  'http://localhost:8081');
+    define('URL_ADMIN_FUTURONERD',  'http://admin.futuronerd.net');
     define('URL_CONSULTA_CEP',      'https://viacep.com.br/ws/');
 ?>
